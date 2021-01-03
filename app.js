e1b32301af716bdc16bf2dd8aff3e5d8b379b1c1
Komal Mann <mannkomal35@gmail.com>
2021-01-03 16:54:36 +0530

    completed

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var button = document.querySelector("#btn")
 var textInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output");
 
-console.log(textInput);
+var ServerURL = "https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationURL(text) {
+    return ServerURL + "?" + "text=" + text;
+}
 
-button.addEventListener("click", function clickEventHandler()
-{
-    console.log("clicked")
-    console.log("input",textInput.value);
-})
+function errorHandler(error) {
+    console.log("Error Occurred: ", error)
+    alert("Something went wrong! Please try again in some time.");
+}
+button.addEventListener("click", function clickEventHandler() {
+    var InputText = textInput.value;
+
+    if(InputText=="")
+    {
+        alert("Please insert some English text first");
+    }
+
+    fetch(getTranslationURL(InputText)).then(response => response.json()).then(json => {
+        outputDiv.innerText = json.contents.translated;
+    }).catch(errorHandler);
+});
